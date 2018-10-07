6543b4e0cbf7567876e6803f4e00e2086d165157
Oernest <bjoern.bohr@googlemail.com>
2018-10-07 15:51:32 +0200

    jquery gefuddel für navigation

--- user/themes/porto-theme/source/javascript/main.js
+++ user/themes/porto-theme/source/javascript/main.js
@@ -1,18 +1,27 @@
-
-/* Add Fadein to Parent*/
-var ids = [
-    "header",
-    "about",
-    "works",
-    "contact"
-];
+var IDs = [];
 
 jQuery(document).ready(function(){
-    jQuery.each( ids, function( i, val ) {
-        jQuery("#" + val).addClass('fadein');
-    });
-    /*  Start text typed  */
+    jQuery("#top").find("header, section, footer").each(function(){
 
+        IDs.push(jQuery(this).attr("id"));
+
+        var filterIds = IDs.filter(function( element ) {
+            return element !== undefined;
+
+        });
+
+        var resultArray = filterIds.join();
+
+        console.log(resultArray);
+
+        jQuery.each( filterIds, function( i, val ) {
+            jQuery("#" + val).addClass('fadein');
+            jQuery(".nav-list").append('<a class="nav-link text text__w text__upper link link__p flex-container flex-container__center-V" href="#'+val+'">' + val + '</a>');
+        });
+
+    });
 });
+
+
 
 
