180a3af61f223aebb28da257c51325bccc049afe
Oernest <bjoern.bohr@googlemail.com>
2018-10-11 19:49:10 +0200

    SCROLL INTO VIEW eingebaut um animation selbst zu setzten

--- user/themes/porto-theme/js/main-debug.js
+++ user/themes/porto-theme/js/main-debug.js
@@ -1,24 +1,33 @@
+
+
+
 var IDs = [];
 
 jQuery(document).ready(function(){
+
+
+
     jQuery("#top").find("header, section, footer").each(function(){
 
         IDs.push(jQuery(this).attr("id"));
 
         var filterIds = IDs.filter(function( element ) {
             return element !== undefined;
-
         });
 
-        var resultArray = filterIds.join();
 
-        console.log(resultArray);
 
         jQuery.each( filterIds, function( i, val ) {
             jQuery("#" + val).addClass('fadein');
-            jQuery(".nav-list").append('<a class="nav-link text text__w text__upper link link__p flex-container flex-container__center-V" href="#'+val+'">' + val + '</a>');
+      /*      jQuery(".nav-list").append('<a class="nav-link text text__w text__upper link link__p flex-container flex-container__center-V" href="#'+val+'">' + val + '</a>');*/
         });
 
+    });
+
+
+
+    jQuery('#about').on('appear', function(event, $all_appeared_elements) {
+        console.log('foobar');
     });
 });
 
--- user/themes/porto-theme/source/javascript/main.js
+++ user/themes/porto-theme/source/javascript/main.js
@@ -1,25 +1,39 @@
+
+
+
 var IDs = [];
 
 jQuery(document).ready(function(){
+
+    function isVisisble(elem){
+        return $(elem).offset().top - $(window).scrollTop() < $(elem).height() ;
+    }
+    $(window).scroll(function(){
+        if (isVisisble( $('#about') )) {
+            $('#about').addClass('turn_on');
+        } else {
+            $('#about').removeClass('turn_on');
+        }
+    });
+
+
     jQuery("#top").find("header, section, footer").each(function(){
 
         IDs.push(jQuery(this).attr("id"));
 
         var filterIds = IDs.filter(function( element ) {
             return element !== undefined;
-
         });
 
-        var resultArray = filterIds.join();
 
-        console.log(resultArray);
 
         jQuery.each( filterIds, function( i, val ) {
             jQuery("#" + val).addClass('fadein');
-            jQuery(".nav-list").append('<a class="nav-link text text__w text__upper link link__p flex-container flex-container__center-V" href="#'+val+'">' + val + '</a>');
+      /*      jQuery(".nav-list").append('<a class="nav-link text text__w text__upper link link__p flex-container flex-container__center-V" href="#'+val+'">' + val + '</a>');*/
         });
 
     });
+
 });
 
 
